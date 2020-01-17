9f0d5cbba414f8a90411daa47a054b10be23749f
RebelliousSpirit <rebelliousspirit111@gmail.com>
2020-01-17 17:10:35 +0600

    some added

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -13,70 +13,79 @@ document.addEventListener("DOMContentLoaded", function() {
 	});
 	$('.main-slider__dots li button').html('');
 	
-	//*dropdown menu in the header
-	var touch = $('.dropdown-btn');
-	var menuWrapper = $('.dropdown');
-	var dropdownMenu = $('.dropdown-menu');
-	var w = $(window).width();
+	/**
+	 * dropdown menu in the header
+	 */
+	(function () {
+		var touch = $('.dropdown-btn');
+		var menuWrapper = $('.dropdown');
+		var dropdownMenu = $('.dropdown-menu');
+		var w = $(window).width();
 
-  	$('html').click(function() {
-    	menuWrapper.find('.dropdown-menu').slideUp();
-    	touch.delay(1600).removeClass('active-btn'); 
-  	});
-	menuWrapper.click(function(e) {
-	   e.stopPropagation();
-	});
-	$(touch).on('click', function(e) {
-	    e.preventDefault();
-	    var menu = $(this).next('.dropdown-menu');
-	    var isClosed = menu.is(':hidden'); 
-	    
-	    menuWrapper.stop(false, true).find('.dropdown-menu').slideUp(); 
-	    touch.delay(1600).removeClass('active-btn'); 
-	   
-	    if (isClosed) {
-	    	menu.slideDown();
-	    	$(this).delay(1600).addClass('active-btn');
-		}else{
-			$(this).delay(1600).removeClass('active-btn');
-		}
+	  	$('html').click(function() {
+	    	menuWrapper.find('.dropdown-menu').slideUp();
+	    	touch.delay(1600).removeClass('active-btn'); 
+	  	});
 
-	});
+		menuWrapper.click(function(e) {
+		   e.stopPropagation();
+		});
 
-	//*dropdown list in the header
-	$('.nav .nav__item').hover(function() {
-		if($(window).width() > 992){
-			$(this).children('.nav__sub-nav').stop(true, true).fadeIn(600);
-		}		
-	}, function() {
-		if($(window).width() > 992){
-			$(this).children('.nav__sub-nav').stop(true, true).fadeOut(600);
-		}
-	});
-	
-	//*star rating appears when hovering over the product image
+		$(touch).on('click', function(e) {
+		    e.preventDefault();
+		    var menu = $(this).next('.dropdown-menu');
+		    var isClosed = menu.is(':hidden'); 
+		    
+		    menuWrapper.stop(false, true).find('.dropdown-menu').slideUp(); 
+		    touch.delay(1600).removeClass('active-btn'); 
+		   
+		    if (isClosed) {
+		    	menu.slideDown();
+		    	$(this).delay(1600).addClass('active-btn');
+			}else{
+				$(this).delay(1600).removeClass('active-btn');
+			}
+
+		});
+	})();	
+
+	/**
+	 * star rating appears when hovering over the product image
+	 */
 	$('.addon-product__thumb-wrap img').hover(function() {
-		let parent = $(this).closest('.addon-product');
+		var parent = $(this).closest('.addon-product');
 		$(parent).children('.star-rating').css({
 			opacity: '1',
 			top: '5px'
 		});
 	}, function() {
-		let parent = $(this).closest('.addon-product');
+		var parent = $(this).closest('.addon-product');
 		$(parent).children('.star-rating').css({
 			opacity: '0',
 			top: '0'
 		});
 	});
 
-	//remove the typical click behavior of an item
+	
+	$('.star-rating__bg').each(function(index, el) {
+		var ratingCount	= $(el).data('rating');
+		var elemWidth = (ratingCount * 20) + "%";
+
+		$(el).css('width', elemWidth);
+	});
+
+	/**
+	 * remove the typical click behavior of an item
+	 */	
 	$('.tooltip-inner').on('click',function(event) {
 		event.preventDefault();
 		event.stopPropagation();
 		/* Act on the event */
 	});
 	
-	//* fixed menu in site header
+	/**
+	 * fixed menu in site header
+	 */
 	$(window).scroll(function(){
 		if ($(this).width() > 523) {
 			 if($(this).scrollTop()>40){
@@ -88,31 +97,54 @@ document.addEventListener("DOMContentLoaded", function() {
 		}       
     });
    
-    //* modal window
+    /**
+     * modla window
+     */
     $('.gallery-item__thumb img').on('click', function(event) {
     	event.preventDefault();
 		createModalWindow();
 
-    	let thisImgSrc = $(this).attr('src');
-    	let title = $(this).attr('alt');
-    	let modalImg = '<figure><img src="" alt="изображение">\
+    	var thisImgSrc = $(this).attr('src');
+    	var title = $(this).attr('alt');
+    	var modalImg = '<figure><img src="" alt="изображение">\
 			<figcaption></figcaption></figure>';
 		
-		$('body').css('overflow', 'hidden');
+		$('.page-holder').css('overflow', 'hidden');
 		$('#modal-window .modal-window__content').prepend(modalImg);
 		$('#modal-window figure>img').attr('src', thisImgSrc);
 		$("#modal-window .modal-window__content figcaption").html(title);
 		$('#modal-window').css({
 			display: 'block'
 		});
-    });    
+    });
+
+    $('.addon-product .addon-quick-view').on('click', function(event) {
+    	event.preventDefault();
+    	createModalWindow();
+
+    	var parent = $(this).closest('.addon-product');
+    	var thisImg = $(parent).children('.addon-product__thumb-wrap').children('a').children('img');
+    	var thisImgSrc = $(thisImg).attr('src');
+    	var title = $(thisImg).attr('alt');
+    	var modalImg = '<figure><img src="" alt="изображение" class="img-responsive">\
+			<figcaption></figcaption></figure>';
+		
+		$('.page-holder').css('overflow', 'hidden');
+		$('#modal-window .modal-window__content').prepend(modalImg);
+		$('#modal-window figure>img').attr('src', thisImgSrc);
+		$("#modal-window .modal-window__content figcaption").html(title);
+		$('#modal-window').css({
+			display: 'block'
+		});
+    	
+    });        
 
     function createModalWindow() {    	
-    	let modalWindow = '<div id="modal-window" class="modal-window">\
+    	var modalWindow = '<div id="modal-window" class="modal-window">\
     	<div class="modal-window__wrap"><div class="modal-window__content">\
     	</div></div></div>';    	
 
-    	$('body').prepend(modalWindow);
+    	$('.page-holder').prepend(modalWindow);
 
     	$('#modal-window').on('click', function(event) {
     		event.preventDefault();
@@ -122,8 +154,44 @@ document.addEventListener("DOMContentLoaded", function() {
     	});
     }
 
-    //* tabs    
+    /**
+     * tabs
+     */
     $('#tab-side-container').easytabs();
+    /**
+     * magnifying glass effect when hovering over an image on the product page
+     */
+   	(function(){
 
+   		var boxWidth = $('.addon-product__image_large').width();
+   		var boxHeight = $('.addon-product__image_large').height();
 
+   		$('.addon-product__image_large img').attr({
+   			width: boxWidth,
+   			height: boxHeight,
+   		});
+
+   	})();    
+
+    $('.loupe-in-img').css({
+    	borderRadius : '50%',
+    });
+    
+   
+	/**
+	 * 
+	 */
+   	$('.addon-product__images-item img').on('click', function () {
+   		var imgSrc = $(this).attr('src');
+   		
+   		$('.addon-product__image_large img').attr({
+   			src : imgSrc,
+   		});
+
+   		$('.loupe-in-img img').attr({
+   			src : imgSrc,
+   		});
+
+   	});
 });
+
---
+++ gulpfile.js
@@ -0,0 +1,153 @@
+var gulp         = require('gulp'),
+		sass         = require('gulp-sass'),
+		browserSync  = require('browser-sync'),
+		concat       = require('gulp-concat'),
+		uglify       = require('gulp-uglify-es').default,
+		cleancss     = require('gulp-clean-css'),
+		autoprefixer = require('gulp-autoprefixer'),
+		rsync        = require('gulp-rsync'),
+		newer        = require('gulp-newer'),
+		rename       = require('gulp-rename'),
+		responsive   = require('gulp-responsive'),
+		del          = require('del');
+
+// Local Server
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app',
+			index: 'product.html'
+		},
+		notify: false,
+		// online: false, // Work offline without internet connection
+		// tunnel: true, tunnel: 'projectname', // Demonstration page: http://projectname.localtunnel.me
+	})
+});
+function bsReload(done) { browserSync.reload(); done(); };
+
+// Custom Styles
+gulp.task('styles', function() {
+	return gulp.src('app/sass/**/*.sass')
+	.pipe(sass({ outputStyle: 'expanded' }))
+	.pipe(concat('styles.min.css'))
+	.pipe(autoprefixer({
+		grid: true,
+		overrideBrowserslist: ['last 10 versions']
+	}))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Optional. Comment out when debugging
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream())
+});
+
+// Scripts & JS Libraries
+gulp.task('scripts', function() {
+	return gulp.src([
+		// 'node_modules/jquery/dist/jquery.min.js', // Optional jQuery plug-in (npm i --save-dev jquery)
+		'app/js/_lazy.js', // JS library plug-in example
+		'app/js/_custom.js', // Custom scripts. Always at the end
+		])
+	.pipe(concat('scripts.js'))	
+	.pipe(gulp.dest('app/js'))
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+// Responsive Images
+var quality = 95; // Responsive images quality
+
+// Produce @1x images
+gulp.task('img-responsive-1x', async function() {
+	return gulp.src('app/img/_src/**/*.{png,jpg,jpeg,webp,raw}')
+		.pipe(newer('app/img/@1x'))
+		.pipe(responsive({
+			'**/*': { width: '50%', quality: quality }
+		})).on('error', function (e) { console.log(e) })
+		.pipe(rename(function (path) {path.extname = path.extname.replace('jpeg', 'jpg')}))
+		.pipe(gulp.dest('app/img/@1x'))
+});
+// Produce @2x images
+gulp.task('img-responsive-2x', async function() {
+	return gulp.src('app/img/_src/**/*.{png,jpg,jpeg,webp,raw}')
+		.pipe(newer('app/img/@2x'))
+		.pipe(responsive({
+			'**/*': { width: '100%', quality: quality }
+		})).on('error', function (e) { console.log(e) })
+		.pipe(rename(function (path) {path.extname = path.extname.replace('jpeg', 'jpg')}))
+		.pipe(gulp.dest('app/img/@2x'))
+});
+gulp.task('img', gulp.series('img-responsive-1x', 'img-responsive-2x', bsReload));
+
+// Clean @*x IMG's
+gulp.task('cleanimg', function() {
+	return del(['app/img/@*'], { force: true })
+});
+
+// Code & Reload
+gulp.task('code', function() {
+	return gulp.src('app/**/*.html')
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+// Deploy
+gulp.task('rsync', function() {
+	return gulp.src('app/')
+	.pipe(rsync({
+		root: 'app/',
+		hostname: 'username@yousite.com',
+		destination: 'yousite/public_html/',
+		// include: ['*.htaccess'], // Included files
+		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excluded files
+		recursive: true,
+		archive: true,
+		silent: false,
+		compress: true
+	}))
+});
+
+gulp.task('watch', function() {
+	gulp.watch('app/sass/**/*.sass', gulp.parallel('styles'));
+	gulp.watch(['libs/**/*.js', 'app/js/_custom.js'], gulp.parallel('scripts'));
+	gulp.watch('app/*.html', gulp.parallel('code'));
+	gulp.watch('app/img/_src/**/*', gulp.parallel('img'));
+});
+
+gulp.task('default', gulp.parallel('img','styles', 'scripts', 'browser-sync', 'watch'));
+
+//*-------------------------------------------------------------------------------------*//
+//*выгружаем готовые файлы в папку продакшн 'dist'
+gulp.task('bild',function (){
+	var bildCustomJs = gulp.src([
+		'app/bower_components/jquery/dist/jquery.min.js',
+		'app/js/custom/*.js'
+	])
+	.pipe(gulp.dest('dist/js'));
+
+	var bildJs = gulp.src([
+		'app/js/scripts.js'
+	])
+	.pipe(uglify()) // Minify js (opt.)
+	.pipe(rename('scripts.min.js'))
+	.pipe(gulp.dest('dist/js'));
+
+	var bildCss = gulp.src([
+		'app/css/**/*.css'
+	])
+	.pipe(gulp.dest('dist/css'));
+
+	var bildFonts = gulp.src('app/fonts/**/*.*')
+	.pipe(gulp.dest('dist/fonts'));
+
+	var bildHtml = gulp.src('app/*.html')
+	.pipe(gulp.dest('dist'));
+
+
+	var bildImgSmall = gulp.src(['app/img/*.*'])
+	.pipe(gulp.dest('dist/img'));
+
+	var bildImgSmall = gulp.src(['app/img/@1x/*.*'])
+	.pipe(gulp.dest('dist/img/@1x'));
+	
+	var bildImgLarge = gulp.src(['app/img/@2x/*.*'])
+	.pipe(gulp.dest('dist/img/@2x'));
+
+
+});
